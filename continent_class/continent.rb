b961af82a21b3b2c65df2b5f0c209e2afac1ecb1
andresrgallo <andresgallo@hotmail.com>
2018-03-12 10:03:29 +1100

    Changes new idea

--- continent_class/continent.rb
+++ continent_class/continent.rb
@@ -1,34 +1,30 @@
-# require_relative 'city'
-# require_relative 'country'
+
 class Continent
-  attr_accessor :name
-  def initialize(name)
+  attr_accessor :name, :population
+  def initialize(name,population)
     @name = name
+    @population = population
     @countries = []
   end
 
-  def add_country(country_name)
-    @countries << country_name.country_name
+  def add_country(country)
+    @countries << country.name
     end
 
   def print_countries
     p @countries
   end
-  def name
-    p @name
-  end
 end
 
 class Country < Continent
-  attr_accessor :country_name
-  def initialize(country_name)
-    
-    @country_name = country_name
-    @states = []
+  attr_accessor :name, :population
+  def initialize(name, population)
+    super(name, population)
+   @states = []
   end
 
   def add_states(state)
-    @states << state.state_name
+    @states << state.name
   end
 
   def print_states
@@ -38,79 +34,44 @@ class Country < Continent
 end
 
 class State < Country
-  attr_accessor :state_name, :cities
   
-  def initialize(state_name)
-    @state_name = state_name
+  def initialize(name, population)
+    super(name,population)
     @cities = []
   end
 
   def add_cities(city)
-    @cities << city.city_name
+    @cities << city.name
   end
   
   def print_cities
     p @cities
   end
-  def state_name
-    @state_name
-  end
 end
 
 class City < State
-  attr_accessor :city_name
-  def initialize(city_name)
-    @city_name = city_name
+  
+  def initialize(name, population)
+    super(name,population)
   end
 end
 
 
 
 
-sudamerica = Continent.new("south america")
-sudamerica.add_country(colombia = Country.new('colombia'))
-bogota = City.new("bogota")
-cundi = State.new('cundi')
+sudamerica = Continent.new("south america",400000000)
+sudamerica.add_country(colombia = Country.new('colombia', 45000000))
+bogota = City.new("bogota", 7500000)
+cundi = State.new("cundi", 10000000)
 cundi.add_cities(bogota)
 colombia.add_states(cundi)
 
-cundi.print_cities
-colombia.print_states
+p sudamerica.population
+p colombia.population
 sudamerica.print_countries
-# puts sudamerica.name
-bogota.print_cities
-# bogota.print_cities
+p bogota.name
+cundi.print_cities 
+colombia.print_states
 
-
-# puts "find the country of a city"
-# gets.chomp
-# bogota.state_name
-    
-
-#Adding states
-
-# colombia_states.each {|states| p sudamerica.print_countries[0]}    #.add_states(states)}
-# venezuela_states = %w[cundi antiq valle santander atlantico]
-# venezuela_states.each {|states| sud_countries[sud_countries.index("venezuela")].add_states(states)}
-# chile_states = %w[cundi antiq valle santander atlantico]
-# chile_states.each {|states| sud_countries[sud_countries.index("chile")].add_states(states)}
-# argentina_states = %w[cundi antiq valle santander atlantico]
-# argentina_states.each {|states| sud_countries[sud_countries.index("argentina")].add_states(states)}
-# brasil_states = %w[cundi antiq valle santander atlantico]
-# brasil_states.each {|states| sud_countries[sud_countries.index("brasil")].add_states(states)}
-# ecuador_states = %w[cundi antiq valle santander atlantico]
-# ecuador_states.each {|states| sud_countries[sud_countries.index("ecuador")].add_states(states)}
-
-
-
-
-
-
-#adding a city
-# puts "Add a city"
-# puts "first what country in South America"
-# p sudamerica.print_countries
-# gets.chomp
-# puts "what state"
 
  
