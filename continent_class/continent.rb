5f98e90a6dea45ac67b2b09b628635fbd3a924de
andresrgallo <andresgallo@hotmail.com>
2018-03-13 22:31:24 +1100

    Modified file, delete superclasses

--- continent_class/continent.rb
+++ continent_class/continent.rb
@@ -8,7 +8,7 @@ class Continent
   end
 
   def add_country(country)
-    @countries << country.name
+    @countries << country
     end
 
   def print_countries
@@ -16,15 +16,16 @@ class Continent
   end
 end
 
-class Country < Continent
+class Country
   attr_accessor :name, :population
   def initialize(name, population)
-    super(name, population)
-   @states = []
+    @name = name
+    @population = population
+    @states = []
   end
 
   def add_states(state)
-    @states << state.name
+    @states << state
   end
 
   def print_states
@@ -33,15 +34,16 @@ class Country < Continent
 
 end
 
-class State < Country
-  
+class State
+  attr_accessor :name, :population
   def initialize(name, population)
-    super(name,population)
+    @name = name
+    @population = population
     @cities = []
   end
 
   def add_cities(city)
-    @cities << city.name
+    @cities << city
   end
   
   def print_cities
@@ -49,11 +51,12 @@ class State < Country
   end
 end
 
-class City < State
-  
+class City
+  attr_accessor :name, :population
   def initialize(name, population)
-    super(name,population)
-  end
+    @name = name
+    @population = population
+   end
 end
 
 
@@ -61,10 +64,20 @@ end
 
 sudamerica = Continent.new("south america",400000000)
 sudamerica.add_country(colombia = Country.new('colombia', 45000000))
+sudamerica.add_country(chile = Country.new('chile', 25000000))
+sudamerica.add_country(venezuela = Country.new('venezuela', 32000000))
 bogota = City.new("bogota", 7500000)
+caracas = City.new("caracas", 7000000)
+santiago = City.new("santiago", 8500000)
 cundi = State.new("cundi", 10000000)
+cara = State.new("cara", 10000000)
+santi = State.new("santi", 10000000)
 cundi.add_cities(bogota)
 colombia.add_states(cundi)
+cara.add_cities(caracas)
+venezuela.add_states(cara)
+santi.add_cities(santiago)
+chile.add_states(santi)
 
 p sudamerica.population
 p colombia.population
@@ -72,6 +85,9 @@ sudamerica.print_countries
 p bogota.name
 cundi.print_cities 
 colombia.print_states
+p sudamerica.inspect
+p bogota.inspect
+
 
 
  
